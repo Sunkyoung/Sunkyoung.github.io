b0dfadf0fa2862849cd1202ad5c0c82c374aef0d
Sunkyoung <sunk10926@gmail.com>
2021-12-12 17:33:06 +0900

    feat: update author info

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `Sun-chive`,
+  description: `우물 밖을 탈출하기 위한 기록`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://sunkyoung.github.io/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `김선경`,
     bio: {
-      role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
+      role: `NLP Researcher`,
+      description: ['데이터 기반의 사고를 가진', '더 나은 세상을 만들고 싶은', '끊임없이 도전하는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/Sunkyoung`, // `https://github.com/zoomKoding`,
+      linkedIn: `https://www.linkedin.com/in/sunkyoung-kim`, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+      email: `sunkyoung@kaist.ac.kr`, // `zoomkoding@gmail.com`,
     },
   },
 
