e8cf7ac685401a12186bcec49e33a1507a42895a
Sunkyoung <sunk10926@gmail.com>
2021-12-19 00:17:20 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-14039d4c45aa4fcd0c31.js"
+    "url": "webpack-runtime-133331eefacf6d8ebc0e.js"
   },
   {
     "url": "styles.03fc299332c95b033e04.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cabc92d43ce03ac2460e776ccd56cbc1"
+    "revision": "08d2343fd80f1d5211f80c1016b20b86"
   },
   {
     "url": "static/webfonts/s/roboto/v29/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
