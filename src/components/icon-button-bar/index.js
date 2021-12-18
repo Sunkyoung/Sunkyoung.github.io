fcf8fb9d8f2dd527fba707e413a10e42bcb73b4d
Sunkyoung <sunk10926@gmail.com>
2021-12-19 00:37:59 +0900

    feat: add resume and cv icon

--- src/components/icon-button-bar/index.js
+++ src/components/icon-button-bar/index.js
@@ -7,6 +7,7 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import AndroidIcon from '@mui/icons-material/Android';
 import AppleIcon from '@mui/icons-material/Apple';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 import './style.scss';
 
@@ -28,8 +29,10 @@ function IconButtonBar({ links = {} }) {
         return <EmailIcon {...props} />;
       case 'linkedIn':
         return <LinkedInIcon {...props} />;
-      case 'CV':
+      case 'koreanResume':
         return <DescriptionIcon {...props} />;
+      case 'englishCV':
+        return <AttachFileIcon {...props} />;
       default:
         return <></>;
     }
