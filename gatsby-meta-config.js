334df290242ccfe169ceb42480baee0fc8701743
Sunkyoung <sunk10926@gmail.com>
2021-12-19 00:43:49 +0900

    feat: add new profile image

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -14,8 +14,8 @@ module.exports = {
     name: `김선경`,
     bio: {
       role: `NLP Researcher`,
-      description: ['데이터 기반의 사고를 가진', '더 나은 세상을 만들고 싶은', '끊임없이 도전하는'],
-      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+      description: ['우물 밖 개구리가 되려는', '데이터 기반의 사고를 가진', '더 나은 세상을 만들고 싶은', '끊임없이 도전하는'],
+      thumbnail: 'mailfrog.jpg', // Path to the image in the 'asset' folder
     },
     social: {
       github: `https://github.com/Sunkyoung`, // `https://github.com/zoomKoding`,
