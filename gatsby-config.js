575d96e7edb4ba5406f46bf3576899a5041bbe70
Sunkyoung <sunk10926@gmail.com>
2022-03-31 15:12:30 +0900

    fix: katex path and gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,7 +118,7 @@ module.exports = {
             resolve: `gatsby-remark-katex`,
             options: {
               // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-              strict: `ignore`,
+              strict: 'ignore',
             },
           },
           `gatsby-remark-autolink-headers`,
